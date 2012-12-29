7d58fb9a191f95ec529468a64f7825cc43a81ba1
prinsmike <mike.prinsloo@gmail.com>
2012-12-29 12:06:01 +0200

    Added a json unmarshal example.

---
+++ json/unmarshal.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Message struct {
+	Name string
+	Body string
+	Time int64
+}
+
+func main() {
+	var m Message
+	b := []byte("{\"Name\":\"Alice\",\"Body\":\"Hello\",\"Time\":1294706395881547000}")
+	err := json.Unmarshal(b, &m)
+	if err != nil {
+		fmt.Println("An error occured!")
+	} else {
+		fmt.Println(m)
+	}
+}
