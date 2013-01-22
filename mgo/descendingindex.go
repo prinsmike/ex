9e2ce61cce3fdf2ef21b798942e7325e35b6d6be
prinsmike <mike.prinsloo@gmail.com>
2013-01-22 11:50:28 +0200

    MongoDB index descending order.

---
+++ mgo/descendingindex.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+)
+
+type Person struct {
+	Name  string
+	Phone string
+}
+
+func main() {
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("test2").C("people")
+
+	index := mgo.Index{
+		// Prefix name with (-) for descending order.
+		Key:        []string{"-name"},
+		Unique:     true,
+		DropDups:   true,
+		Background: true,
+		Sparse:     true,
+	}
+	err = c.EnsureIndex(index)
+
+	_ = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Cla", "+55 53 8402 8510"})
+
+	result := []Person{}
+	err = c.Find(bson.M{}).All(&result)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result)
+}
