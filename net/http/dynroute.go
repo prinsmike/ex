20477c9e7d6c489e6efbdbed21b461658722ede4
Michael Prinsloo <mike.prinsloo@gmail.com>
2014-07-28 00:16:04 +0200

    Cleanup.

--- net/http/dynroute.go
+++ net/http/dynroute.go
@@ -12,30 +12,24 @@ type Path struct {
 
 var pages = make(map[Path]string)
 
-func main() {
-
-	pages[Path{"localhost:8080", "/"}] = "<h1>Welcome</h1>"
-	pages[Path{"localhost:8080", "/test"}] = "<h1>Test</h1>"
-
-	http.HandleFunc("/", servFunc)
-
-	err := http.ListenAndServe(":8080", nil)
-	checkError(err)
-}
-
-func servFunc(w http.ResponseWriter, req *http.Request) {
-	host := req.Host
-	path := req.URL.Path
-	if content, ok := pages[Path{host, path}]; ok {
+func httpHandler(w http.ResponseWriter, req *http.Request) {
+	if content, ok := pages[Path{req.Host, req.URL.Path}]; ok {
 		fmt.Fprintf(w, content)
 	} else {
 		fmt.Fprintf(w, "Could not find the requested URL")
 	}
 }
 
-func checkError(err error) {
-	if err != nil {
-		fmt.Println("Fatal error ", err.Error())
-		os.Exit(1)
+func main() {
+
+	pages[Path{"localhost:8080", "/"}] = "<h1>Welcome</h1>"
+	pages[Path{"localhost:8080", "/test"}] = "<h1>Test</h1>"
+	pages[Path{"test.locl", "/test"}]
+
+	http.HandleFunc("/", httpHandler)
+
+	e := http.ListenAndServe(":8080", nil)
+	if e != nil {
+		fmt.Println("Could not start web server!")
 	}
 }
