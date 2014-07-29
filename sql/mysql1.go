3283f415c088999f01d2bd49b10c0576d4d1c306
Michael Prinsloo <mike.prinsloo@gmail.com>
2014-07-29 16:54:31 +0200

    Added a simple mysql example.

---
+++ sql/mysql1.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "super:SO9OG1g7GYe4eslA@tcp(127.0.0.1:3306)/test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
