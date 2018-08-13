7922ae861cd8c2b5cb47e5bc601c7a0ca0c5965a
Michael Prinsloo <mike.prinsloo@gmail.com>
2018-08-13 15:32:49 +0200

    Screenshots.

--- extern/screenshot/main.go
+++ extern/screenshot/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image/png"
+	"log"
 	"os"
 
 	"github.com/kbinani/screenshot"
@@ -10,15 +11,20 @@ import (
 
 func main() {
 	n := screenshot.NumActiveDisplays()
+	log.Printf("Active Displays: %d\n", n)
 
 	for i := 0; i < n; i++ {
 		bounds := screenshot.GetDisplayBounds(i)
+		log.Printf("Bounds: %#v\n", bounds)
 		img, err := screenshot.CaptureRect(bounds)
 		if err != nil {
 			panic(err)
 		}
 		fileName := fmt.Sprintf("%d_%dx%d.png", i, bounds.Dx(), bounds.Dy())
-		file, _ := os.Create(fileName)
+		file, err := os.Create(fileName)
+		if err != nil {
+			panic(err)
+		}
 		defer file.Close()
 		png.Encode(file, img)
 		fmt.Printf("#%d : %v \"%s\"\n", i, bounds, fileName)
